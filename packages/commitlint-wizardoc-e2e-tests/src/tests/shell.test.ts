d6be0adba526a3a0d5c4a7539a2e55f8a621b0ae
赵晓晨 <825316969@qq.com>
2021-06-02 22:31:46 +0800

    [Fix::test] fixbug for shell test

--- packages/commitlint-wizardoc-e2e-tests/src/tests/shell.test.ts
+++ packages/commitlint-wizardoc-e2e-tests/src/tests/shell.test.ts
@@ -2,7 +2,6 @@
 import { $ } from "../utils/shell";
 
 const SUCCESS_SIGNAL = 0;
-const ERROR_SIGNAL = 1;
 
 describe("branchNameTest", () => {
   it("success branch name", () => {
@@ -14,24 +13,24 @@ describe("branchNameTest", () => {
   it("type error", () => {
     const res = $`sh ./.github/shell/branchName.sh test/git-rebase-test`;
 
-    expect(res.code).toEqual(ERROR_SIGNAL);
+    expect(res.code).not.toEqual(SUCCESS_SIGNAL);
   });
 
   it("empty branch name error", () => {
     const res = $`sh ./.github/shell/branchName.sh feature/`;
 
-    expect(res.code).toEqual(ERROR_SIGNAL);
+    expect(res.code).not.toEqual(SUCCESS_SIGNAL);
   });
 
   it("separator branch name error", () => {
     const res = $`sh ./.github/shell/branchName.sh feature/brach_name`;
 
-    expect(res.code).toEqual(ERROR_SIGNAL);
+    expect(res.code).not.toEqual(SUCCESS_SIGNAL);
   });
 
   it("upper Case branch name error", () => {
     const res = $`sh ./.github/shell/branchName.sh feature/brachName`;
 
-    expect(res.code).toEqual(ERROR_SIGNAL);
+    expect(res.code).not.toEqual(SUCCESS_SIGNAL);
   });
 });
